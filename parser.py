7efa5bdca2f9e39d3fd4e3bcfec891431a9edfe3
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-06-20 14:05:14 -0400

    first working prototype with multiple levels

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
 import csv
 import json
 import pandas as pd
+from collections import defaultdict
 from pprint import pprint
 
 index = []
-hier = {} 
+hier = {'name': 'index',
+        'children': []} 
 
 with open('miller.txt', 'r') as f1:
     data = csv.reader(f1, delimiter='\t')
@@ -18,8 +20,6 @@ for row in index:
     else:
         pass
 
-#pprint(index)
-
 for item in index:
     pages = []
     if item[2] != None:
@@ -28,38 +28,16 @@ for item in index:
                 if item[1] in row:
                     pages.append(row[2])
             pages = list(set(pages))
-        hier[item[0]] = {item[1]: pages}
+        hier['children'].append({'name': item[0],
+                                 'children': []}) 
+        hier['children'][-1]['children'].append({'name': item[1],
+                                             'children': []})
+        hier['children'][-1]['children'][-1]['children'].append({'name': ', '.join(pages)})
     else:
-        #hier[item[0]] = item[1]
-        pass
+        print('passing')
+        pass 
 
 pprint(hier)
-
-
-#df = pd.DataFrame(hier)
-#df.set_index(['level1', 'level2'], inplace=True)
-#df = df.groupby(['level1']).reset_index()
-#hier = df.groupby(['level1', 'level2'])['level3'].apply(list)
-
-"""
-for item in index:
-    if len(item) == 3:
-        if {item[0]: {item[1]: [item[2]]}} not in hier:
-            hier.append({item[0]: {item[1]: [item[2]]}})
-        else:
-            print('else case')
-            hier[-1:].append(item[2])
-#    elif len(item) == 2:
-#        if item[0] not in hier:
-#            hier[item[0]] = [item[1]]
-#        else:
-#            hier[item[0]].append(item[1])
-
-hier = {'name': 'index',
-        'children': hier}
-
-#pprint(hier)
-"""
 
 with open('index.json', 'w') as f2:
     json.dump(hier, f2)
