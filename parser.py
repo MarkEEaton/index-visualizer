b132a6d986ee692d5ade37a1031a0f4cfe07b197
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-06-20 19:11:14 -0400

    fixed one of the duplication problems

--- parser.py
+++ parser.py
@@ -1,7 +1,5 @@
 import csv
 import json
-import pandas as pd
-from collections import defaultdict
 from pprint import pprint
 
 index = []
@@ -21,6 +19,7 @@ for row in index:
     else:
         pass
 
+
 for item in index:
     pages = []
     if item[2] != None:
@@ -37,21 +36,23 @@ for item in index:
             hier['children'][-1]\
                 ['children'][-1]\
                 ['children'].append({'name': page, 'size': 1})
-    else:
-        for row in index:
-            if item[0] in row:
-                pages.append(row[1])
-            pages = sorted(list(set(pages)))
-        hier['children'].append({'name': item[0],
-                                 'children': []}) 
-        for page in pages:
-            hier['children'][-1]['children'].append({'name': page,
-                                                     'size': 1})
 
-pprint(hier)
+    else:
+        pass
+
+json_check = []
+json_final = []
+
+for item in hier['children']:
+    json_check.append(json.dumps(item))
+
+json_set = sorted(list(set(json_check)))
+
+for item in json_set:
+   json_final.append(json.loads(item))
+
+hier['children'] = json_final
+
 
 with open('index.json', 'w') as f2:
     json.dump(hier, f2)
-
-
-
