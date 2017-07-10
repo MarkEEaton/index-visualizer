721714118bf325807f62c52de197ed6473b2f0ed
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-07-10 18:26:58 -0400

    all working except with some duplication in level 3

--- parser.py
+++ parser.py
@@ -26,46 +26,34 @@ def rectangulate():
             pass
 
 
-def hierarchify3():
+def hierarchify():
     """ create the hierarchy for items with 3 levels"""
-    for item in index:
-        pages3 = []
-        if item[2] != None:
-            for row3 in index:
-                if item[0] in row3:
-                    if item[1] in row3:
-                        pages3.append(row3[2])
-                pages3 = sorted(list(set(pages3)))
-            hier['children'].append({'name': item[0],
-                                     'children': []}) 
-            hier['children'][-1]['children'].append({'name': item[1],
-                                                     'children': []})
-            for page in pages3:
-                hier['children'][-1]\
-                    ['children'][-1]\
-                    ['children'].append({'name': page, 'size': 1})
-    
-        else:
-            """
-            for row2 in index:
-                if item[0] in row2:
-                    pages2.append(row2[1])
-                pages2 = sorted(list(set(pages2)))
-            hier['children'].append({'name': item[0],
-                                     'children': []})
-            print(pages2)
-            for page in pages2:
-                hier['children'][-1]\
-                    ['children'].append({'name': item[1], 
-                                         'size': 1})
-            #pprint(hier)
-            """
 
-def hierarchify2():
+    # make the basic structure
     for item in index:
-        [d['children'].append({'name': item[1], 'size': 1}) for d in hier['children'] if d['name'] in item[0] and item[2] == None]
+        hier['children'].append({'name': item[0],
+                                 'children': []}) 
+
+    deduplicate()
+
+    for item in index:
+
+        # add the two-level items
+        if item[2] == None:
+            [d['children'].append({'name': item[1], 'size': 1}) for d in hier['children'] if d['name'] == item[0] and item[2] == None]
+
+# all good up to here...
+
+        # add the three-level items
+        if item[2] != None:
+            [d['children'].append({'name': item[1], 'children': [{'name': item[2], 'size': 0}]}) for d in hier['children'] if d['name'] == item[0]]
+
+        else:
+            pass
+
 
 def deduplicate():
+    """ dedup hier by converting to json then making a set """
     json_check = []
     json_final = []
     
@@ -81,33 +69,38 @@ def deduplicate():
 
 
 def amalgamate():
-    names = []
     amalgamated = []
 
     for item in hier['children']:
-        names.append(item['name'])
-    names = list(set(names))
-    for item in hier['children']:
+        names = []
+        for subitem in item['children']:
+            names.append(subitem['name'])
+        names = list(set(names))
+        print(names)
+        
         level1 = item['name']
         level2 = item['children'][0]['name']
-        level3 = item['children'][0]['children'][0]
-        list_of_names = [x['name'] for x in new_hier['children']] 
-        if item['name'] not in list_of_names:
-            new_hier['children'].append(item)
-        else:
-            [x['children'].append({'name': level2, 'children': [level3]})\
-             if level1 == x['name'] else 100000000\
-             for x in new_hier['children']]
-            pass
 
+        # replace code below with a way to sub in a unified subentry
+
+        #for subitem in item['children']:
+        #    if 
+"""
+        list_of_names = [x['name'] for x in new_hier['children']] 
+            if item['name'] not in list_of_names:
+                new_hier['children'].append(item)
+            else:
+                [x['children'].append({'name': level2, 'children': [level3]})\
+                 if level1 == x['name'] else 100000000\
+                 for x in new_hier['children']]
+                pass
+"""
 
 if __name__ == '__main__':
     rectangulate()
-    hierarchify3()
-    hierarchify2()
-    pprint(hier)
-    deduplicate()
-    amalgamate()
+    hierarchify()
+    #amalgamate()
+    #pprint(hier)
 
 with open('index.json', 'w') as f2:
-    json.dump(new_hier, f2)
+    json.dump(hier, f2)
