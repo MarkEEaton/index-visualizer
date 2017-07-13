11a24358594d5641447ddd6ea322470c6e2460b3
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-07-13 01:40:45 -0400

    docstrings, flake8, passing hier to recursion

--- parser.py
+++ parser.py
@@ -1,15 +1,15 @@
 import csv
 import json
-from copy import copy
 from pprint import pprint
 
 index = []
 hier = {'name': 'index',
-        'children': []} 
+        'children': []}
 new_hier = {'name': 'index',
-            'children': []} 
+            'children': []}
 
 
+""" create an index list, ignoring see and see also references """
 with open('miller.txt', 'r') as f1:
     data = csv.reader(f1, delimiter='\t')
     for row in data:
@@ -32,21 +32,24 @@ def hierarchify():
     # make the basic structure
     for item in index:
         hier['children'].append({'name': item[0],
-                                 'children': []}) 
+                                 'children': []})
 
     deduplicate()
 
     for item in index:
 
         # add the two-level items
-        if item[2] == None:
-            [d['children'].append({'name': item[1], 'size': 1}) for d in hier['children'] if d['name'] == item[0] and item[2] == None]
-
-        # all good up to here...
+        if item[2] is None:
+            [d['children'].append({'name': item[1], 'size': 1})
+             for d in hier['children'] if d['name'] == item[0] and
+             item[2] is None]
 
         # add the three-level items
-        if item[2] != None:
-            [d['children'].append({'name': item[1], 'children': [{'name': item[2], 'size': 0}]}) for d in hier['children'] if d['name'] == item[0]]
+        if item[2] is not None:
+            [d['children'].append({'name': item[1],
+                                   'children': [{'name': item[2],
+                                                 'size': 0}]})
+             for d in hier['children'] if d['name'] == item[0]]
 
         else:
             pass
@@ -56,37 +59,43 @@ def deduplicate():
     """ dedup hier by converting to json then making a set """
     json_check = []
     json_final = []
-    
+
     for item in hier['children']:
         json_check.append(json.dumps(item))
-    
+
     json_set = sorted(list(set(json_check)), key=str.lower)
-    
+
     for item in json_set:
-       json_final.append(json.loads(item))
-    
+        json_final.append(json.loads(item))
+
     hier['children'] = json_final
 
 
 def yield_subitem():
+    """ iterate through subitems and yield the current one """
     for item in hier['children']:
         for subitem in item['children']:
-            yield subitem
+            yield subitem, item
 
 
 def amalgamate():
+    """ set up the amalgamation """
     past_subitem = {'name': 'blah'}
     ys = yield_subitem()
     while True:
         create_subitem = past_subitem
         try:
-            subitem = next(ys)
+            subitem, item = next(ys)
         except StopIteration:
             return
-        past_subitem = individual_amalgamate(subitem, past_subitem, create_subitem, ys)
+        past_subitem = individual_amalgamate(subitem,
+                                             past_subitem,
+                                             create_subitem,
+                                             ys)
 
 
 def individual_amalgamate(subitem, past_subitem, create_subitem, ys):
+    """ amalgamate the individual items. Recurse when needed. """
     try:
         if subitem['name'] != past_subitem['name']:
             pass
@@ -94,14 +103,10 @@ def individual_amalgamate(subitem, past_subitem, create_subitem, ys):
             create_subitem['children'].append(subitem['children'][0])
             past_subitem = subitem
             try:
-                subitem = next(ys)
+                subitem, item = next(ys)
             except StopIteration:
-                return 
-            print(create_subitem)
-            try:
-                individual_amalgamate(subitem, past_subitem, create_subitem, ys)
-            except StopIteration:
-                pass
+                return
+            individual_amalgamate(subitem, past_subitem, create_subitem, ys)
     except Exception as e:
         print('exception: ' + e)
     finally:
@@ -113,7 +118,6 @@ if __name__ == '__main__':
     rectangulate()
     hierarchify()
     amalgamate()
-    #pprint(hier)
 
 with open('index.json', 'w') as f2:
     json.dump(hier, f2)
